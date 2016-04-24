27bc08b1ded0f301183fbd0ca6745c4e7abff829
tahong <tahongshen@gmail.com>
2016-04-24 16:07:49 +0800

    use browser-sync and add js active page effet

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,28 +2,48 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
+gulp.task('browser-sync', ['build', 'sass', 'cp'], function() {
+  browserSync({
+    server: {
+      baseDir: '..'
+    }
+  });
+});
+
+gulp.task('cp', function () {
+  return gulp.src('js/main.js', { base: '.' })
+         .pipe(gulp.dest('..'));
+});
+
 gulp.task('build', function() {
   gulp.src('pages/*.html')
       .pipe(wrap({src: "layout/default.html"}))
       .pipe(gulp.dest('..'));
 });
+
+gulp.task('rebuild', ['build'], function() {
+  browserSync.reload();
+});
 gulp.task('sass', function() {
   gulp.src('styles/*.scss')
       .pipe(sass()).on('error', handleError)
       .pipe(prefix())
-      .pipe(gulp.dest('../styles'));
+      .pipe(gulp.dest('../styles'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('watch', function() {
-  gulp.watch(['**/*.html'], ['build']);
+  gulp.watch(['**/*.html'], ['rebuild']);
   gulp.watch(['styles/*.scss'], ['sass']);
+  gulp.watch(['js/main.js'], ['cp']);
 });
 
-gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
