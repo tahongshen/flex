f72b17bd5492b08825cca3673ce2ffdf4f8db4f8
tahong <tahongshen@gmail.com>
2016-04-24 15:08:25 +0800

    use gulp-wrap to management layout files

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
+
+gulp.task('build', function() {
+  gulp.src('pages/*.html')
+      .pipe(wrap({src: "layout/default.html"}))
+      .pipe(gulp.dest('..'));
+});
 gulp.task('sass', function() {
   gulp.src('styles/*.scss')
       .pipe(sass()).on('error', handleError)
@@ -13,14 +20,10 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('../styles'));
 });
 
-gulp.task('cp', function() {
-  gulp.src('*.html')
-      .pipe(gulp.dest('../'));
-});
 
 gulp.task('watch', function() {
-  gulp.watch(['*.html'], ['cp']);
+  gulp.watch(['**/*.html'], ['build']);
   gulp.watch(['styles/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['sass', 'cp', 'watch']);
+gulp.task('default', ['sass', 'build', 'watch']);
